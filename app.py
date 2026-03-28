1006b7ad022c86b79317adb0f8331a87020e27e4
kssmrockad0507-gif <kssmrockad0507@gmail.com>
2026-03-28 23:57:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🔥 KSSM Python Server Running!"
+    return "KSSM AI Running"
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/assistant", methods=["POST"])
+def assistant():
+    data = request.json
+    command = data["command"].lower()
+
+    if "time" in command:
+        reply = str(datetime.datetime.now())
+
+    elif "hello" in command:
+        reply = "Hello Santhosh! I am your assistant."
+
+    else:
+        reply = "Command not understood"
+
+    return jsonify({"reply": reply})
